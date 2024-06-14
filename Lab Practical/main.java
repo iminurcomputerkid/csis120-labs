1af4235390b8d5d0987fe8abdd0d476a73ec1aa6
iminurcomputerkid <167721852+iminurcomputerkid@users.noreply.github.com>
2024-06-14 00:31:25 -0400

    Create main.java

---
+++ Lab Practical/main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        System.out.println("Running test cases...");
+
+       // LabPracticalDriver.testWarmUpMethods();
+        LabPracticalDriver.testArrayUtilityMethods();
+         LabPracticalDriver.testHotPlateMethods();
+        //LabPracticalDriver.testChallengeMethod();
+
+    }
+}
