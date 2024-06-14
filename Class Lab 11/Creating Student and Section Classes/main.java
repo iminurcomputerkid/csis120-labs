fb589aa443d8a764b4a49cd82d73a8599b61b1ea
iminurcomputerkid <167721852+iminurcomputerkid@users.noreply.github.com>
2024-06-14 00:21:32 -0400

    Create main.java

---
+++ Class Lab 11/Creating Student and Section Classes/main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Beginning Tests");
+
+        StudentDriver.testOne();
+        StudentDriver.testTwo();
+        StudentDriver.testThree();
+        SectionDriver.testOne();
+        SectionDriver.testTwo();
+        SectionDriver.testThree();        
+
+    }
+}
